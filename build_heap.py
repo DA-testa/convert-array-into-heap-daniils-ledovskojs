3f0dde111e5d381611366b5af1deea6805dfcc3e
daniils-ledovskojs <125797151+daniils-ledovskojs@users.noreply.github.com>
2023-03-19 19:33:04 +0000

    done

--- build_heap.py
+++ build_heap.py
@@ -1,42 +1,54 @@
-# python3
+# 221RDB300
 
 
 def build_heap(data):
+    n = len(data)
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
-
-
+    for i in range(n // 2, -1, -1):
+        min_heapify(i, data, swaps)
     return swaps
 
+def min_heapify(i, data, swaps):
+    n = len(data)
+    min_index = i
+    left_child = 2 * i + 1
+    if left_child < n and data[left_child] < data[min_index]:
+        min_index = left_child
+    right_child = 2 * i + 2
+    if right_child < n and data[right_child] < data[min_index]:
+        min_index = right_child
+    if i != min_index:
+        swaps.append((i, min_index))
+        data[i], data[min_index] = data[min_index], data[i]
+        min_heapify(min_index, data, swaps)
 
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
+    cmd = input()
+    if "F" in cmd:
+        file_path = input()
+        path = "tests/"+file_path
+        text = open(path)
+        text1 = text.read()
+        text.close()
+        sep = text1.partition("\n")
+        n = int(sep[0])
+        data = sep[2].split(" ")
+        assert len(data) == n
+        swaps = build_heap(data)
+        print(len(swaps))
+        for i, j in swaps:
+            print(i, j)
 
 
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
 
-    # checks if lenght of data is the same as the said lenght
-    assert len(data) == n
-
-    # calls function to assess the data 
-    # and give back all swaps
-    swaps = build_heap(data)
-
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
-    print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
-
+    elif "I" in cmd:
+        n = int(input())
+        data = list(map(int, input().split()))
+        assert len(data) == n
+        swaps = build_heap(data)
+        print(len(swaps))
+        for i, j in swaps:
+            print(i, j) 
 
 if __name__ == "__main__":
-    main()
+    main()       
